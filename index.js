0d440442551938d0ef7a0d4175e3d94e758d839d
Geoff Kimball <gakimball@bsu.edu>
2015-12-03 22:44:58 -0800

    Add compile times and final status message

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var sass = require('node-sass');
 var cssDiff = require('css-diff');
 var rimraf = require('rimraf').sync;
 var fs = require('fs');
+var colors = require('colors');
 
 module.exports = function(options, cb) {
   var sc = new SassCompare(options);
@@ -14,6 +15,7 @@ module.exports = function(options, cb) {
 
 function SassCompare(options) {
   this.options = extend({}, options);
+  this.messages = '';
 }
 
 /**
@@ -57,6 +59,7 @@ SassCompare.prototype.cleanup = function(cb) {
 SassCompare.prototype.compileRuby = function(cb) {
   var _this = this;
   var args = [this.options.input, './.sass-compare/ruby.css'];
+  var time = process.hrtime();
   var rubySass = spawn('sass', args);
 
   rubySass.stderr.on('data', function(data) {
@@ -73,6 +76,9 @@ SassCompare.prototype.compileRuby = function(cb) {
       return '';
     }
 
+    var compileTime = (process.hrtime(time)[1] / 1000000).toFixed(2);
+    var message = '\n    Ruby Sass took ' + compileTime + 'ms to compile.';
+    _this.messages += message.green;
     cb();
   });
 }
@@ -81,7 +87,9 @@ SassCompare.prototype.compileRuby = function(cb) {
  * Uses Libsass (via node-sass) to compile the input Sass file to `.sass-compare/libsass.css`.
  */
 SassCompare.prototype.compileLibsass = function(cb) {
+  var _this = this;
   var outputPath = path.join(process.cwd(), '.sass-compare', 'libsass.css');
+  var time = process.hrtime();
 
   sass.render({
     file: this.options.input
@@ -90,6 +98,10 @@ SassCompare.prototype.compileLibsass = function(cb) {
       console.log('Libsass ran into this error:\n' + err);
       cb(err);
     }
+
+    var compileTime = (process.hrtime(time)[1] / 1000000).toFixed(2);
+    var message = '\n    Libsass took ' + compileTime + 'ms to compile.\n';
+    _this.messages += message.green;
 
     fs.writeFile(outputPath, data.css, function() {
       cb(err, data);
@@ -101,10 +113,11 @@ SassCompare.prototype.compileLibsass = function(cb) {
  * Compares the two CSS files saved to `.sass-compare`.
  */
 SassCompare.prototype.compare = function(cb) {
+  var _this = this;
   var filePath = path.join(process.cwd(), '.sass-compare');
   var files = [
-    path.join(filePath, 'ruby.css'),
-    path.join(filePath, 'libsass.css')
+    path.join(filePath, 'libsass.css'),
+    path.join(filePath, 'ruby.css')
   ];
 
   cssDiff({
@@ -114,9 +127,10 @@ SassCompare.prototype.compare = function(cb) {
   }).then(function(diff) {
     if (diff.different) {
       console.log(diff.visual);
+      _this.messages += '\n    Found differences! Red lines show where Libsass is different.'.red;
     }
     else {
-      console.log('No differences found.');
+      _this.messages += '\n    No differences found.'.green;
     }
 
     cb();
@@ -127,5 +141,6 @@ SassCompare.prototype.compare = function(cb) {
  * TODO: pretty-print the errors, comparison, etc.
  */
 SassCompare.prototype.report = function(cb) {
+  console.log(this.messages + '\n');
   cb();
 }
